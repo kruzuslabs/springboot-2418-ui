58cbb6d5c52217c785cbac7e4c3993301d853686
Bek <kruzus@live.com>
2024-01-17 12:56:08 -0500

    finished basic setup with nav

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import "@/styles/globals.scss"
+
+import Navbar from "../components/Navbar";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Task Manager - The perfect tool to manage your tasks and improve your productivity. Create, organize and complete your tasks easily." />
+        <meta name="keywords" content="task manager, productivity, create tasks, complete tasks, organize tasks" />
+        <meta name="author" content="kruzusLabs" />
+        <title>Task Manager - Manage Your Tasks</title>
+      </Head>
+      <Navbar />
+      <main className="container mx-auto px-8 py-8 bg-base-200 rounded-md">
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
